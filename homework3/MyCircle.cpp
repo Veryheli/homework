f709c7ed4c431b9ad249a24e2735c626fe59d2e0
Chenrenh <1771391789@qq.com>
2021-09-09 17:33:25 +0800

    第二次作业

--- homework3/MyCircle.cpp
+++ homework3/MyCircle.cpp
@@ -11,10 +11,11 @@ public:
     float getR();
     float getCir();
     float getArea();
+    ~MyCircle();
 };
 MyCircle::MyCircle():r(0){};
 MyCircle::MyCircle(float r):r(r){};
-void MyCircle::setR(float r)
+void MyCircle::setR(float r)//类外定义要加上作用域
 {
     this->r = r;
 }
@@ -32,10 +33,38 @@ float MyCircle::getArea()
 }
 int main()
 {
-    MyCircle *c1 = new MyCircle(5);
+    cout<<"请输入圆c1和圆c2的半径"<<endl;
+    float rc1,rc2;
+    cin>>rc1>>rc2;
+    MyCircle *c1 = new MyCircle(rc1);
     MyCircle *c2 = new MyCircle();
-    c2->setR(2.5);
-   //TODO 
+    c2->setR(rc2);
+    cout<<"c1的面积为"<<c1->getArea()<<" c2的面积为"<<c2->getArea()<<endl;
+    cout<<"c1的周长为"<<c1->getCir()<<" c2的周长为"<<c2->getCir()<<endl;
+    if(c1->getArea() > c2->getArea())
+    {
+        cout<<"c1圆的面积更大"<<endl;
+    }
+    else if(c1->getArea() == c2->getArea())
+    {
+        cout<<"两个圆的面积一样大"<<endl;
+    }
+    else
+    {
+        cout<<"c2圆的面积更大"<<endl;
+    }
+    if(c1->getCir() > c2->getCir())
+    {
+        cout<<"c1圆的周长更大"<<endl;
+    }
+    else if(c1->getArea() == c2->getArea())
+    {
+        cout<<"两个圆的周长一样大"<<endl;
+    }
+    else
+    {
+        cout<<"c2圆的周长更大"<<endl;
+    }
     return 0;
 }
 
