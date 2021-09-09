0f69b3144ecd26b61815472c9fae444cf30e75e0
Chenrenh <1771391789@qq.com>
2021-09-09 19:35:38 +0800

    remove

--- homework4/Stock.cpp
+++
@@ -1,4 +0,0 @@
-#include <iostream>
-#include "Stock.h"
-using namespace std;
-//TODO
--- homework4/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Stock.h"
-using namespace std;
-int main()
-{
-    //TODO
-    return 0;
-}
-
